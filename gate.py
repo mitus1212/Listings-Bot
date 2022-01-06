2ba78e86820c35e508fab45b0f58104de7cce0a1
MateuszRadej <95027636+MateuszRadej@users.noreply.github.com>
2022-01-06 18:27:28 +0100

    Update gate.py

--- gate.py
+++ gate.py
@@ -3,12 +3,28 @@ import datetime
 from bs4 import BeautifulSoup
 import urllib.request
 # open a connection to a URL using urllib
-from urllib.request import Request, urlopen
+from urllib.request import Request
+import sqlite3
+
 bot = "2147060992:AAE3Sy6l2LYTHkgv0V5B_WokZBBPS87zn0s"
 chat_id = "1437944800"
 
-lista=[]
-lista_2=[]
+
+connection = sqlite3.connect('listingi.db')
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS listingi
+              (title TEXT)''')
+connection.commit()
+
+
+
+
+connection.commit()
+
+cursor.execute("SELECT * FROM listingi")
+fetch = cursor.fetchall()
+print(fetch)
+
     
 url = 'https://www.kucoin.com/news/1'
 
@@ -29,24 +45,24 @@ def send_message(chat_id="1437944800"):
     content = requests.get(URL)   
     soup = BeautifulSoup(content.text, 'html.parser')
     parameters = {"chat_id": chat_id}
-    if len(lista) > 50:
+    '''if len(lista) > 50:
         del lista[:25]
     elif len(lista_2) > 50:
         del lista_2[:25]
     else:
-        pass
+        pass'''
 
     rows = soup.find_all('h3')
     for row in rows:
         new_row = str(row)
-        if "offering" in new_row and new_row not in lista:
-            lista.append(new_row)
+        if "offering" in new_row and ("{new_row}".format(new_row=new_row),) not in fetch:
+            cursor.execute("INSERT INTO listingi VALUES ('{text}')".format(text=new_row))
             message = requests.post("https://api.telegram.org/bot"+ bot + "/sendMessage?chat_id=" + chat_id + "&text=https://www.gate.io/articlelist/ann?fbclid=IwAR2rXyAloIwsWW4xMnDCte__zDY495MMxdxcqGR7IqP1ffdZ-yMlapTXRkQ", data=parameters)
-            print(datetime.datetime.now()) 
-        elif "Offering" in new_row and new_row not in lista:
-            lista.append(new_row)
+            connection.commit()
+        elif "Offering" in new_row and ("{new_row}".format(new_row=new_row),) not in fetch:
+            cursor.execute("INSERT INTO listingi VALUES ('{text}')".format(text=new_row))
             message = requests.post("https://api.telegram.org/bot"+ bot + "/sendMessage?chat_id=" + chat_id + "&text=https://www.gate.io/articlelist/ann?fbclid=IwAR2rXyAloIwsWW4xMnDCte__zDY495MMxdxcqGR7IqP1ffdZ-yMlapTXRkQ", data=parameters)
-            print(datetime.datetime.now())
+            connection.commit()
         else:
             pass
     
@@ -58,14 +74,17 @@ def send_message(chat_id="1437944800"):
     rows_2 = soup_2.find_all('div')
     for row in rows_2:
         new_row = str(row)
-        if "Will List" in new_row and new_row not in lista_2:
-            lista_2.append(new_row)
+        if "Will List" in new_row and ("{new_row}".format(new_row=new_row),) not in fetch:
+            cursor.execute("INSERT INTO listingi VALUES ('{text}')".format(text=new_row))
             message = requests.post("https://api.telegram.org/bot"+ bot + "/sendMessage?chat_id=" + chat_id + "&text=https://www.binance.com/en/support/announcement/c-48?navId=48", data=parameters)
-            print(datetime.datetime.now())  
+            connection.commit()
         else:
             pass
-    print(len(lista))
-    print(len(lista_2))
+    #cursor.execute("DELETE FROM listingi WHERE title LIKE '%<h3> Gate.io Startup Initial Offering: Solvent(SVT) </h3>%'")
+    #connection.commit()
+
+    cursor.close()
+    connection.close()
 
 
 send_message("1437944800")
